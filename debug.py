16f572bf1f1b72af40ef28db52eaf4cb04391bd7
Andrew Zimolzak <andyzimolzak@gmail.com>
2013-11-25 09:47:51 -0500

    Working Print command

---
+++ debug.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# Simple debugger
+import sys
+import readline
+
+def remove_html_markup(s):
+    tag   = False
+    quote = False
+    out   = ""
+
+    for c in s:
+        if c == '<' and not quote:
+            tag = True
+        elif c == '>' and not quote:
+            tag = False
+        elif c == '"' or c == "'" and tag:
+            quote = not quote
+        elif not tag:
+            out = out + c
+    return out
+
+# main program that runs the buggy program
+def main():
+    print remove_html_markup('xyz')
+    print remove_html_markup('"<b>foo</b>"')
+    print remove_html_markup("'<b>foo</b>'")
+
+# globals
+breakpoints = {9: True, 14: True}
+stepping = False
+
+def debug(command, my_locals):
+    global stepping
+    global breakpoints
+
+    if command.find(' ') > 0:
+        arg = command.split(' ')[1]
+    else:
+        arg = None
+
+    if command.startswith('s'):     # step
+        stepping = True
+        return True
+    elif command.startswith('c'):   # continue
+        stepping = False
+        return True
+    elif command.startswith('q'):   # quit
+        sys.exit(0)
+    else:
+        print "No such command", repr(command)
+
+    return False
+
+commands = ["s", "s", "q"]
+
+def input_command():
+    #command = raw_input("(my-spyder) ")
+    global commands
+    command = commands.pop(0)
+    return command
+
+def traceit(frame, event, trace_arg):
+    global stepping
+
+    if event == 'line':
+        if stepping or breakpoints.has_key(frame.f_lineno):
+            resume = False
+            while not resume:
+                print event, frame.f_lineno, frame.f_code.co_name, frame.f_locals
+                command = input_command()
+                resume = debug(command, frame.f_locals)
+    return traceit
+
+sys.settrace(traceit)
+
+print remove_html_markup('xyz')
