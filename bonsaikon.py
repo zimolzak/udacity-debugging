7a4fb6d65c0c455b16d40ab95df123093e7a884a
Andrew Zimolzak <andyzimolzak@gmail.com>
2013-12-11 18:29:23 -0500

    Prompt file for Pset 2, part 2

--- bonsaikon.py
+++ bonsaikon.py
@@ -91,7 +91,7 @@ def traceit(frame, event, arg):
 sys.settrace(traceit)
 # Tester. Increase the range for more precise results when running locally
 eps = 0.000001
-for i in range(1, 10):
+for i in range(1, 10000):
     r = int(random.random() * 1000) # An integer value between 0 and 999.99
     z = square_root(r, eps)
     z = square(z)
